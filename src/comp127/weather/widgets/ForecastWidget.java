6d6f4f93a836c521a5961944e88ba6da70f05edd
karynasteele <ksteele1@macalester.edu>
2024-10-26 17:11:57 -0500

    Updated forecast widget

--- src/comp127/weather/widgets/ForecastWidget.java
+++ src/comp127/weather/widgets/ForecastWidget.java
@@ -7,13 +7,23 @@ import edu.macalester.graphics.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.awt.Color;
+
 public class ForecastWidget implements WeatherWidget {
 
     private final double size;
     private GraphicsGroup group;
+
     private GraphicsText temperature;
+    private GraphicsText highLow;
     private GraphicsText caption;
+    private GraphicsText date;
+    private GraphicsText time;
     private Image image;
+
+    private double boxWidth;
+    private double boxHeight;
+    private double boxPadding;
     // TODO: Add instance variables for any UI elements that you create in the constructor but
     //       will need to update later with new data
 
@@ -26,13 +36,30 @@ public class ForecastWidget implements WeatherWidget {
 
         group = new GraphicsGroup();
 
-        GraphicsText temperature = new GraphicsText();
+        temperature = new GraphicsText();
+        temperature.setFont(FontStyle.BOLD, size * 0.08);
         group.add(temperature);
 
-        GraphicsText caption = new GraphicsText();
+        highLow = new GraphicsText();
+        highLow.setFont(FontStyle.PLAIN, size * 0.08);
+        highLow.setFillColor(Color.GRAY);
+        group.add(highLow);
+
+        caption = new GraphicsText();
+        caption.setFont(FontStyle.PLAIN, size * 0.06);
         group.add(caption);
 
-        Image image = new Image(0,0);
+        date = new GraphicsText();
+        date.setFont(FontStyle.BOLD, size * 0.07);
+        group.add(date);
+
+        time = new GraphicsText();
+        time.setFont(FontStyle.BOLD, size * 0.07);
+        group.add(time);
+
+        image = new Image(0,0);
+        image.setMaxHeight(size * 0.2);
+        image.setMaxWidth(size * 0.2);
         group.add(image);
 
         // TODO: Create the fixed text and image elements you will need (but not the row of boxes
@@ -41,6 +68,10 @@ public class ForecastWidget implements WeatherWidget {
 
         boxGroup = new GraphicsGroup();
         group.add(boxGroup);
+
+        boxWidth = size * 0.04;
+        boxHeight = size * 0.06;
+        boxPadding = size * 0.01;
 
         updateLayout();
     }
@@ -64,9 +95,6 @@ public class ForecastWidget implements WeatherWidget {
         //     past the end of the current one.
         //   - Add the new box to the graphics group
         //   - Add the new box to the `boxes` list
-        double boxWidth = size * 0.1;
-        double boxHeight = size * 0.2;
-        double boxSpacing = size * 0.02;
         double rightMargin = size * 0.05;
         double x = 0;
         double y = 0;
@@ -76,11 +104,10 @@ public class ForecastWidget implements WeatherWidget {
             boxGroup.add(box);
             boxes.add(box);
 
-            x += boxWidth + boxSpacing;
+            x += boxWidth + boxPadding;
             if(x + boxWidth > size - rightMargin) {
-
                 x = 0;
-                y += boxHeight + 2 * boxSpacing;
+                y += boxHeight + 2 * boxPadding;
             }  
         }
 
@@ -97,14 +124,31 @@ public class ForecastWidget implements WeatherWidget {
     private void selectForecast(ForecastBox box) {
         // TODO: Call `setActive` for all the forecast boxes, with `true` for the selected box and
         //       `false` for all the others (so that the previously active box becomes inactive).
-        for(ForecastBox forecastBox : boxes) {
-            forecastBox.setActive(forecastBox == box);
+        for(ForecastBox currentBox : boxes) {
+            if(currentBox.equals(box)) {
+                currentBox.setActive(true);
+            }
+            else {
+                currentBox.setActive(false);
+            }
         }
 
         // TODO: Get the forecast data from the box, and use it to update the text and icon.
-        temperature.setText((box.getForecast().getTemperature() + "\u2109"));
-        caption.setText(box.getForecast().getWeatherDescription());
-        image.setImagePath(box.getForecast().getWeatherIcon());
+        ForecastConditions conditions = box.getForecast();
+
+        temperature.setText((FormattingHelpers.checkForNull(conditions. getTemperature()) + "\u2109"));
+
+        highLow.setText(FormattingHelpers.checkForNull(conditions.getMinTemperature()) + "\u2109" + " | " +
+        FormattingHelpers.checkForNull(conditions.getMaxTemperature()) + "\u2109");
+
+        caption.setText(conditions.getWeatherDescription());
+
+        date.setText(FormattingHelpers.WEEKDAY_AND_NAME.format(conditions.getPredictionTime()));
+
+        time.setText(FormattingHelpers.TIME_OF_DAY.format(conditions.getPredictionTime()));
+
+        image.setImagePath(conditions.getWeatherIcon());
+
         updateLayout();
     }
 
@@ -114,11 +158,19 @@ public class ForecastWidget implements WeatherWidget {
         //             different widget sizes.
         //       HINT: Study the methods of `GraphicsObject` to find different ways of positioning
         //             and measuring graphics objects
-        double padding = size * 0.05; // Padding around the elements
-        double iconHeight = size * 0.4; // Height for the icon (40% of widget size)
-        double labelHeight = size * 0.1; // Height for the label (10% of widget size)
-        double descriptionHeight = size * 0.05; // Height for the description (5% of widget size)
+        temperature.setCenter(size * 0.5, size * 0.5);
 
+        highLow.setCenter(size * 0.5, size * 0.6);
+
+        caption.setCenter(size * 0.5, size * 0.7);
+
+        date.setCenter(size * 0.25, size * 0.08);
+
+        time.setCenter(size * 0.8, size * 0.075);
+
+        image.setCenter(size * 0.5, size * 0.3);
+
+        boxGroup.setCenter(size * 0.5, size * 0.9);
     }
 
     /**
@@ -146,5 +198,8 @@ public class ForecastWidget implements WeatherWidget {
         //       HINT: Study the methods above! They will help you immensely.
         //       HINT: This should be a small method. If it gets complicated,
         //             you‘ve gone off the rails.
+        if(getBoxAt(position) != null) {
+            selectForecast(getBoxAt(position));
+        }
     }
 }
